ecf65219637151c9fd1fbb4fc64c681307eadcf4
NikiforosTs <64402467+NikiforosTs@users.noreply.github.com>
2021-05-18 16:32:39 +0300

    Add files via upload

---
+++ datagen.py
@@ -0,0 +1,46 @@
+import random
+import numpy as np
+from datetime import datetime
+
+### Generating the features
+
+### m is the number of datapoints and N is the number of features
+def data_generator(m, N):
+
+    combined = []
+
+    x_array = np.random.uniform(0, 1, (m, N))
+    x = x_array / np.linalg.norm(x_array)
+
+    ### Generating the classes
+    y = []
+    values = [1, -1]
+
+    for i in range(m):
+        y.append(random.choice(values))
+
+    combined = np.column_stack((x, y))
+
+   # with open('datagen.txt', 'w') as file:
+   #     for item in combined:
+   #         file.write('%s\n' % item)
+
+    with open('datagen.txt', 'w') as file:
+        for item in combined:
+            s = " ".join(map(str, item))
+            file.write(s + '\n')
+
+    return combined
+
+### To validate that the eucledian norm equals to 1
+#euclideanNorm = x.T.dot(x)
+#euclideanNorm
+
+start_time = datetime.now()
+
+### Specify number of datapoints and features
+data_generator(1000, 3000)
+
+end_time = datetime.now()
+
+print('Duration: {}'.format(end_time - start_time))
