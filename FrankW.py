8ad2d97c13ecdc5bf1798bff70b6941b0f117263
Zkeal <zkeal@outlook.com>
2021-05-19 23:25:35 +0200

    fix converge problem

--- FrankW.py
+++ FrankW.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pylab as plt
 import random
+import scipy.linalg as sc
 
 
 # block decent - for Frank wolfre
 def FW_BD(x, y, point = None, max_iter=200, tolerance=1e-100, partition = 3):
+    # x is training data, point is viarable that we update
     if point is None:
-        point = np.random.randn(x.shape[1], 1) * 1
+        point = np.random.randn(x.shape[1], 1) * 0.0001
         #point = np.array([233., 1924., 182., -1191., -575., 643., 205., 385., 1028., 199.]).T
     point = point.reshape(x.shape[1], 1)
     blocks_index = get_blocks(x,partition = partition)
@@ -46,11 +48,11 @@ def FW_BD(x, y, point = None, max_iter=200, tolerance=1e-100, partition = 3):
 #     return point, gaps, loses
 
 
-#adaptive FrankWofle for self - concordant
 def FrankWolfewithSC(x, y, point = None, max_iter=1000, tolerance=1e-100):
     M = 2  ##to be determined
     if point is None:
-        point = np.random.randn(x.shape[1], 1) * 10
+        #target_vector
+        point = np.random.randn(x.shape[1], 1) * 0.0001
         #point = np.array([[233., 1924., 182., -1191., -575., 643., 205., 385., 1028., 199.]]).T
     gaps = []
     loses = []
@@ -61,8 +63,9 @@ def FrankWolfewithSC(x, y, point = None, max_iter=1000, tolerance=1e-100):
         if abs(gap) < tolerance:
             break
         #Hssian = Hessian_LRgradient(x)
-        e = M/2 * np.square(np.dot(np.dot(Hssian, point).T, point))
-        tk = gap / (e * (gap + (4 * e)/(M ** M)))
+        e = M/2 * np.square(Hssian)
+        # tk = gap / (e * (gap + (4 * e)/(M ** M)))
+        tk = gap / (gap * M * e /2 + e ** 2)
         ak = min(1, float(tk))
         point = point + ak * vk
     return point, gaps, loses
@@ -97,17 +100,15 @@ def FWSC_state_logloss(x,y, theta):
     if GAP< 0:
         print("error GAP")
     squeeze_gap = np.squeeze(GAP)
-    Hssian = hess_logloss(x, exp_p, y)
+    Hssian = hess_logloss(x, exp_p, y, s - theta)
     return float(squeeze_gap), s-theta, loss, Hssian
 
 
-def linear_oracle(grad, r = 1):
-    s = np.zeros(grad.shape)
+def linear_oracle(grad, coe = 0.05):
+    s = np.zeros(grad.shape[0])
     i_max = np.argmax(np.abs(grad))
-    s[i_max] = -r * np.sign(grad[i_max]) # 1 x n
-    return s
-
-
+    s[i_max] = -(grad.shape[0] * coe)*np.sign(grad [i_max])
+    return s.reshape(grad.shape)
 
 def LR(x):
     return 1.0 / (1.0 + np.exp(-x))
@@ -130,11 +131,6 @@ def loss_function(x, y, theta):
 
 
 def log_loss(x, y, theta, gamma = 1e-4):
-    """
-           theta -- N x n
-           y -- N x 1
-           x -- 1 x n
-    """
     exp_p = np.exp(-y * (x.dot(theta)))
     log_loss = np.sum(np.log(1 + exp_p))/x.shape[0] + gamma/2 * np.linalg.norm(theta,2)**2
     return log_loss, exp_p
@@ -143,14 +139,14 @@ def log_loss(x, y, theta, gamma = 1e-4):
 def gradient_logloss(x, y, theta, exp_product, gamma = 1e-4):
     p1 = -y * exp_product / (exp_product + 1)
     KK = x.T @ p1
-    gradient = KK + gamma * theta
+    gradient = KK/x.shape[0] + gamma * theta
     return gradient
 
 
-def hess_logloss(x, exp_p, y, gamma = 1e-4):
-    p1 = -y * exp_p / (exp_p + 1)
-    g1 = x.T @ p1
-    hess = g1.T @ g1 /x.shape[0] + gamma * np.eye(x.shape[1])
+def hess_logloss(x, exp_p, y, s , gamma = 1e-4):
+    p1 = y * exp_p / (exp_p + 1)
+    g1 = x @ s
+    hess = (g1.T@p1)**2 /x.shape[0] + gamma*np.linalg.norm(s,2)**2
     return hess
 
 def get_blocks(x, partition = 3):
--- LR.py
+++ LR.py
@@ -1,12 +1,8 @@
 import numpy as np
-from scipy.optimize import fmin_tnc
-from sklearn.linear_model import LogisticRegression
+
 import re
 import FrankW
 from datagen import data_generator
-from sklearn.cluster import Kmeans
-from yellowbrick.cluster import KElbowVisualizer
-from datatime import datatime
 
 class LogisticR:
     def __init__(self, data_path):
@@ -15,21 +11,21 @@ class LogisticR:
         self.dimension_S = 1
         self.dimension_E = 11
 
-    def kernel(self, x, activation ="sigmoid"):
-        if activation == "sigmoid":
-            return 1.0 / (1.0 + np.exp(-x))
-
-    def loss_function(self, x, y, theta):
-        y_1 = 1. / (1. + np.exp(-x.dot(theta)))
-        return - np.sum(y * np.log(y_1) + (1 - y) * np.log(1 - y_1)) / len(y)
-        # total_loss = -(1/size) * np.sum(np.log(self.kernel(np.dot(x, theta))) * y + (1-y) * np.log(1-self.kernel(np.dot(x, theta))))
-        # return total_loss
-
-    ### baseline for the compare the result
-    def gradient(self, theta, x, y):
-        size = x.shape[0]
-        return (1 / size) * np.dot(x.T, self.kernel(np.dot(x, theta)) - y)
-
+    # def kernel(self, x, activation ="sigmoid"):
+    #     if activation == "sigmoid":
+    #         return 1.0 / (1.0 + np.exp(-x))
+    #
+    # def loss_function(self, x, y, theta):
+    #     y_1 = 1. / (1. + np.exp(-x.dot(theta)))
+    #     return - np.sum(y * np.log(y_1) + (1 - y) * np.log(1 - y_1)) / len(y)
+    #     # total_loss = -(1/size) * np.sum(np.log(self.kernel(np.dot(x, theta))) * y + (1-y) * np.log(1-self.kernel(np.dot(x, theta))))
+    #     # return total_loss
+    #
+    # ### baseline for the compare the result
+    # def gradient(self, theta, x, y):
+    #     size = x.shape[0]
+    #     return (1 / size) * np.dot(x.T, self.kernel(np.dot(x, theta)) - y)
+    #
     def load_data(self):
         x = []
         y = []
@@ -47,26 +43,26 @@ class LogisticR:
         x,y = data_generator(shape[0], shape[1])
         return np.array(x), np.array(y).reshape(len(y), 1)
 
-    def process_gtd(self):
-        #x, y = self.load_data()
-        x, y = self.generate_data()
-        self.parameters['theta'] = np.random.randn(x.shape[1], 1) * 0.1
-        model, loss = self.LRGD(x, y, 1000)
-        result = self.kernel(np.dot(x, model))
-        y_predict = np.array(result >= 0.5, dtype='int')
-        FrankW.display_performance(loss, "GD", "loss")
-        return result
-
-    def process_fw(self):
-        #x, y = self.load_data()
-        x,y = self.generate_data()
-        point, gaps, losses = FrankW.FrankWolfe(10, x, y, max_iter= 1000)
-        FrankW.display_performance(gaps, "FW", "FW_GAPS")
-        FrankW.display_performance(gaps, "FW", "FW_Losses")
+    # def process_gtd(self):
+    #     #x, y = self.load_data()
+    #     x, y = self.generate_data()
+    #     self.parameters['theta'] = np.random.randn(x.shape[1], 1) * 0.1
+    #     model, loss = self.LRGD(x, y, 1000)
+    #     result = self.kernel(np.dot(x, model))
+    #     y_predict = np.array(result >= 0.5, dtype='int')
+    #     FrankW.display_performance(loss, "GD", "loss")
+    #     return result
+    #
+    # def process_fw(self):
+    #     #x, y = self.load_data()
+    #     x,y = self.generate_data()
+    #     point, gaps, losses = FrankW.FrankWolfe(10, x, y, max_iter= 1000)
+    #     FrankW.display_performance(gaps, "FW", "FW_GAPS")
+    #     FrankW.display_performance(gaps, "FW", "FW_Losses")
 
     def process_fwcs(self):
-        x, y = self.load_data()
-        #x, y = self.generate_data()
+        #x, y = self.load_data()
+        x, y = self.generate_data()
         point, gaps, losses = FrankW.FrankWolfewithSC(x, y)
         FrankW.display_performance(gaps, "FW_SC", "GAPS")
         FrankW.display_performance(losses, "FW_SC", "Losses")
@@ -78,25 +74,24 @@ class LogisticR:
         FrankW.display_performance(gaps, "FW_BD", "FWBD_GAPS")
         FrankW.display_performance(losses, "FW_BD", "FWBD_Losses")
 
-    def LRGD(self, x, y, maxIteration, learning_rate = 0.1):
-        n = np.shape(x)[0]
-        theta = self.parameters['theta']
-        iteration = 0
-        y= y.reshape((n,1))
-        performance = []
-        while iteration <= maxIteration:
-            theta = theta - learning_rate * self.gradient(theta, x, y)
-            iteration += 1
-            loss = self.loss_function(x, y, theta)
-            performance.append(loss)
-            if iteration % 100 == 0:
-                print("iteration", str(iteration), "error rate:：", str(loss))
-        return theta, performance
+    # def LRGD(self, x, y, maxIteration, learning_rate = 0.1):
+    #     n = np.shape(x)[0]
+    #     theta = self.parameters['theta']
+    #     iteration = 0
+    #     y= y.reshape((n,1))
+    #     performance = []
+    #     while iteration <= maxIteration:
+    #         theta = theta - learning_rate * self.gradient(theta, x, y)
+    #         iteration += 1
+    #         loss = self.loss_function(x, y, theta)
+    #         performance.append(loss)
+    #         if iteration % 100 == 0:
+    #             print("iteration", str(iteration), "error rate:：", str(loss))
+    #     return theta, performance
 
 
 if __name__ == '__main__':
-    
-    start_time = datetime.now()
+
     
     # if you need change the file path, edit here
     LogR = LogisticR('covtype_bin')
@@ -111,8 +106,5 @@ if __name__ == '__main__':
 
     #Block with FW
     #test2 = LogR.process_BDfwcs()
-    test2 = LogR.process_BDfwcs()
-    
-    end_time = datetime.now()
+    #test2 = LogR.process_BDfwcs()
 
-    print('Duration: {}'.format(end_time - start_time))
